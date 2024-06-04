554557d7210f1597fb2aa4e831d1b6e3992ecc05
lucas-iribaren <lucas.iribaren@laplateforme.io>
2024-06-04 13:18:15 +0200

    [ajout][(job01)]: les variables

---
+++ jour01/job01/index.php
@@ -0,0 +1,22 @@
+<?php
+
+$lpf = 'LaPlateforme';
+echo $lpf;
+
+
+$vive = 'Vive';
+$exclamation = '!';
+echo $vive . ' ' . $lpf. ' ' . $exclamation . "\n";
+
+$number = 6;
+echo $number;
+
+$number += 4;
+echo $number;
+
+$boleen = true;
+
+echo $boleen;
+
+$boleen = false;
+echo $boleen;
